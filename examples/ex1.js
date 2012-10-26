75c5909c6ec7a6c6170a84973885eebe80dd3e62
Craig Condon <craig@spiceapps.com>
2012-10-25 23:13:51 -0500

    initial commit

--- examples/ex1.js
+++ examples/ex1.js
@@ -1,21 +1,23 @@
-### Example
-
-```javascript
 var express     = require("express"),
 beanpoll        = require("beanpoll"),
-beanpollConnect = require("beanpoll-connect"); 
+beanpollConnect = require("../"); 
 
 
 var router = beanpoll.router();
 
 router.on({
+	// "pull parseBody": express.bodyParser(),
 	"pull auth": function(req, res) {
+		console.log(req.query)
 		if(req.query.user == "user" && req.query.pass == "pass") {
 			this.next();
+		} else {
+			res.end("auth failed!");
+			// res.error("fail");
 		}
 	},
 	"pull -method=GET auth -> saveProfile": function(req, res) {
-		res.end("success!");
+		res.end({ name: "success!" });
 	}
 });
 
@@ -24,5 +26,3 @@ var server = express();
 server.use(beanpollConnect.middleware(router));
 
 server.listen(8080); //access saveProfile from the web
-
-```
